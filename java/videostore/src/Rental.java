b3dcfd028be3afdc1d825c8722954bbf3761f46f
Matt Glover <matthewglover@me.com>
2017-07-21 19:54:44 +0100

    Cleans up Rental

--- java/videostore/src/Rental.java
+++ java/videostore/src/Rental.java
@@ -7,14 +7,6 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
-    public int daysRented() {
-        return daysRented;
-    }
-
-    public Movie movie() {
-        return movie;
-    }
-
     public int points() {
         int frequentRenterPoints = 1;
 
@@ -25,7 +17,7 @@ public class Rental {
 
     public double price() {
         double cost = 0;
-        switch (movie().getPriceCode()) {
+        switch (movie.getPriceCode()) {
             case Movie.REGULAR:
                 cost = getRegularRentalAmount();
                 break;
@@ -40,30 +32,29 @@ public class Rental {
     }
 
     public String statement() {
-        return "\t" + movie().getTitle() + "\t"
-                + price();
+        return "\t" + movie.getTitle() + "\t" + price();
     }
 
     private boolean isBonusFrequentRenterPoints() {
-        return movie().getPriceCode() == Movie.NEW_RELEASE
-                && daysRented() > 1;
+        return movie.getPriceCode() == Movie.NEW_RELEASE
+                && daysRented > 1;
     }
 
     private double getChildrensRentalAmount() {
         double rentalAmount = 1.5;
-        if (daysRented() > 3)
-            rentalAmount += (daysRented() - 3) * 1.5;
+        if (daysRented > 3)
+            rentalAmount += (daysRented - 3) * 1.5;
         return rentalAmount;
     }
 
     private double getNewReleaseRentalAmount() {
-        return daysRented() * 3;
+        return daysRented * 3;
     }
 
     private double getRegularRentalAmount() {
         double rentalAmount = 2;
-        if (daysRented() > 2)
-            rentalAmount += (daysRented() - 2) * 1.5;
+        if (daysRented > 2)
+            rentalAmount += (daysRented - 2) * 1.5;
         return rentalAmount;
     }
 }
