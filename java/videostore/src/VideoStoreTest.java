ec365c3162c1eccf2ac4c650ea416046ee1387a9
Matt Glover <matthewglover@me.com>
2017-07-22 07:10:48 +0100

    Cleans up tests

--- java/videostore/src/VideoStoreTest.java
+++ java/videostore/src/VideoStoreTest.java
@@ -3,6 +3,7 @@ import junit.framework.*;
 public class VideoStoreTest extends TestCase
 {
 
+	private RentalRecord rentalRecord;
 	private Movie newRelease1;
 	private Movie newRelease2;
 	private Movie childrens;
@@ -10,43 +11,63 @@ public class VideoStoreTest extends TestCase
 	private Movie regular2;
 	private Movie regular3;
 
-	public VideoStoreTest (String name) {
-		super (name);
+	public VideoStoreTest(String name) {
+		super(name);
 	}
 
-	protected void setUp ()  {
-		rentalRecord = new RentalRecord("Fred");
-		newRelease1 = new NewRelease("The Cell", Movie.NEW_RELEASE);
-		newRelease2 = new NewRelease("The Tigger Movie", Movie.NEW_RELEASE);
-		childrens = new ChildrensMovie("The Tigger Movie", Movie.CHILDRENS);
-		regular1 = new RegularMovie("Plan 9 from Outer Space", Movie.REGULAR);
-		regular2 = new RegularMovie("8 1/2", Movie.REGULAR);
-		regular3 = new RegularMovie("Eraserhead", Movie.REGULAR);
+	protected void setUp()  {
+		rentalRecord = new RentalRecord("Customer");
+		newRelease1 = new NewRelease("New Release 1", Movie.NEW_RELEASE);
+		newRelease2 = new NewRelease("New Release 2", Movie.NEW_RELEASE);
+		childrens = new ChildrensMovie("Childrens Movie", Movie.CHILDRENS);
+		regular1 = new RegularMovie("Regular Movie 1", Movie.REGULAR);
+		regular2 = new RegularMovie("Regular Movie 2", Movie.REGULAR);
+		regular3 = new RegularMovie("Regular Movie 3", Movie.REGULAR);
 	}
 
-	public void testSingleNewReleaseStatement () {
-		rentalRecord.addRental (new Rental (newRelease1, 3));
-		assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", rentalRecord.statement ());
+	public void testNewReleaseStatement() {
+		rentalRecord.addRental(new Rental(newRelease1, 3));
+		
+		assertEquals("Rental Record for Customer\n" +
+				"\tNew Release 1\t9.0\n" +
+				"You owed 9.0\n" +
+				"You earned 2 frequent renter points\n",
+				rentalRecord.statement());
 	}
 
-	public void testDualNewReleaseStatement () {
-		rentalRecord.addRental (new Rental (newRelease1, 3));
-		rentalRecord.addRental (new Rental (newRelease2, 3));
-		assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", rentalRecord.statement ());
+	public void testNewReleaseStatements() {
+		rentalRecord.addRental(new Rental(newRelease1, 3));
+		rentalRecord.addRental(new Rental(newRelease2, 3));
+
+		assertEquals("Rental Record for Customer\n" +
+				"\tNew Release 1\t9.0\n" +
+				"\tNew Release 2\t9.0\n" +
+				"You owed 18.0\n" +
+				"You earned 4 frequent renter points\n",
+				rentalRecord.statement());
 	}
 
-	public void testSingleChildrensStatement () {
-		rentalRecord.addRental (new Rental (childrens, 3));
-		assertEquals ("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", rentalRecord.statement ());
+	public void testChildrensStatement() {
+		rentalRecord.addRental(new Rental(childrens, 3));
+
+		assertEquals("Rental Record for Customer\n" +
+				"\tChildrens Movie\t1.5\n" +
+				"You owed 1.5\n" +
+				"You earned 1 frequent renter points\n",
+				rentalRecord.statement());
 	}
 
-	public void testMultipleRegularStatement () {
-		rentalRecord.addRental (new Rental (regular1, 1));
-		rentalRecord.addRental (new Rental (regular2, 2));
-		rentalRecord.addRental (new Rental (regular3, 3));
+	public void testRegularStatements() {
+		rentalRecord.addRental(new Rental(regular1, 1));
+		rentalRecord.addRental(new Rental(regular2, 2));
+		rentalRecord.addRental(new Rental(regular3, 3));
 
-		assertEquals ("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", rentalRecord.statement ());
+		assertEquals("Rental Record for Customer\n" +
+				"\tRegular Movie 1\t2.0\n" +
+				"\tRegular Movie 2\t2.0\n" +
+				"\tRegular Movie 3\t3.5\n" +
+				"You owed 7.5\n" +
+				"You earned 3 frequent renter points\n",
+				rentalRecord.statement());
 	}
-
-	private RentalRecord rentalRecord;
 }
