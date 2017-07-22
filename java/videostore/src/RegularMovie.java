a46989d9d8fef5f42c1b0b773bf75ce3793aa21e
Matt Glover <matthewglover@me.com>
2017-07-22 08:08:32 +0100

    Extract supplement method in RegularMovie

--- java/videostore/src/RegularMovie.java
+++ java/videostore/src/RegularMovie.java
@@ -1,14 +1,21 @@
 public class RegularMovie extends Movie {
+
+    private static final int BASE_PRICE = 2;
+    private static final double SUPPLEMENTARY_RATE = 1.5;
+    private static final int SUPPLEMENT_THRESHOLD = 2;
+
     public RegularMovie(String title, int priceCode) {
         super(title, priceCode);
     }
 
     @Override
     public double price(int daysRented) {
-        double rentalAmount = 2;
-        if (daysRented > 2) {
-            rentalAmount += (daysRented - 2) * 1.5;
-        }
-        return rentalAmount;
+        return BASE_PRICE + supplement(daysRented);
+    }
+
+    private double supplement(int daysRented) {
+        return daysRented > SUPPLEMENT_THRESHOLD
+                ? (daysRented - SUPPLEMENT_THRESHOLD) * SUPPLEMENTARY_RATE
+                : 0;
     }
 }
